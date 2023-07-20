eefa5d0d7861aff961d9506f8574c4d9ccd54a7c
Shahadat-Murshed <shahadat.murshed@gmail.com>
2023-07-20 20:30:59 +0600

    Front End

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Aripods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devises wirelessly High-Quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iphone 11 pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Intorducing the iPhone 11 pro. A trasnformative triple-camera system that adds tons of capability without complexity, An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 1.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Canon E0S 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Intorducing the iPhone 11 pro. A trasnformative triple-camera system that adds tons of capability without complexity, An unprecedented leap in battery life",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 7,
+    rating: 5.0,
+    numReviews: 8,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 5",
+    image: "/images/playstation.jpg",
+    description:
+      "Intorducing the iPhone 11 pro. A trasnformative triple-camera system that adds tons of capability without complexity, An unprecedented leap in battery life",
+    brand: "Sony",
+    category: "Electronics",
+    price: 500.99,
+    countInStock: 7,
+    rating: 0.5,
+    numReviews: 8,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Intorducing the iPhone 11 pro. A trasnformative triple-camera system that adds tons of capability without complexity, An unprecedented leap in battery life",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 50.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 8,
+  },
+  {
+    _id: "6",
+    name: "Amazon Echo Dot",
+    image: "/images/alexa.jpg",
+    description:
+      "Intorducing the iPhone 11 pro. A trasnformative triple-camera system that adds tons of capability without complexity, An unprecedented leap in battery life",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 50.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 8,
+  },
+];
+
+export default products;
